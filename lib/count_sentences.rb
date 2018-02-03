2265f159cc5c7571bc6c543705575ef3a1f90ba4
Eden Mesfun <emesfun@gmail.com>
2018-02-03 20:57:25 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
-
 class String
 
+  attr_accessor
+  attr_reader :string
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #note to self - sentence splitting occurs once - need a conditional that iterates over the string until it is parsed at every point there is a .,! or ?
+    self.split
+    self.count(".!?")
   end
-end
+
+end #class
