b7781d2399a5ecd126394a65063909a05a963473
Eden Mesfun <emesfun@gmail.com>
2018-02-03 21:07:43 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,6 +20,7 @@ class String
     #note to self - sentence splitting occurs once - need a conditional that iterates over the string until it is parsed at every point there is a .,! or ?
     self.split(/[.!?]/).delete("")
     self.count(".!?")
+    binding.pry
   end
 
 end #class
